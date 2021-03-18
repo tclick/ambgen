132ffb6a37be2598baccc1a76affe62ffa18c865
Timothy Click <tclick@okstate.edu>
2021-03-18 16:39:51 -0500

    test: add code to test rms2d subcommand

--- tests/commands/test_rms2d.py
+++ tests/commands/test_rms2d.py
@@ -12,13 +12,88 @@
 #  TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 #  THIS SOFTWARE.
 # --------------------------------------------------------------------------------------
+import logging
+import sys
+from pathlib import Path
+
+import numpy as np
 import pytest
+import pytraj as pt
+
+from ambgen.cli import main
+
+from ..datafile import TOPWW, TRJWW
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+LOGGER = logging.getLogger(name="ambgen.commands.cmd_rms2d")
+
+if not sys.warnoptions:
+    import os
+    import warnings
+
+    warnings.simplefilter("default")  # Change the filter in this process
+    os.environ["PYTHONWARNINGS"] = "default"  # Also affect subprocesses
 
 
-class TestCase:
-    def test_something(self):
-        assert True is False
+class TestRms2d:
+    @pytest.fixture
+    def data(self):
+        topology = pt.load_topology(TOPWW)
+        trajectory = pt.iterload(TRJWW, top=topology)
+        N_FRAMES = trajectory.n_frames
+        return np.random.random((N_FRAMES, N_FRAMES))
 
+    @pytest.mark.runner_setup
+    def test_help(self, cli_runner, tmp_path: Path):
+        """
+        GIVEN the rms2d subcommand
+        WHEN the help option is invoked
+        THEN the help output should be displayed
+        """
+        result = cli_runner.invoke(
+            main,
+            args=(
+                "rms2d",
+                "-h",
+            ),
+            env=dict(AMBERHOME=tmp_path.as_posix()),
+        )
 
-if __name__ == "__main__":
-    pytest.main()
+        assert "Usage:" in result.output
+        assert result.exit_code == 0
+
+    @pytest.mark.runner_setup
+    def test_calculate_rms2d(self, cli_runner, mocker, tmp_path, data):
+        """
+        GIVEN a topology and a trajectory file
+        WHEN the rms2d subcommand is called
+        THEN calculate the 2D r.m.s.d. of a trajectory
+        """
+        logfile = tmp_path.joinpath("rms2d.log")
+        outfile = tmp_path.joinpath("out.csv")
+        img_file = outfile.with_suffix(".png")
+
+        patch = mocker.patch.object(pt, "rms2d", return_value=data)
+        result = cli_runner.invoke(
+            main,
+            args=(
+                "rms2d",
+                "-s",
+                TOPWW,
+                "-f",
+                TRJWW,
+                "-o",
+                outfile.as_posix(),
+                "-l",
+                logfile.as_posix(),
+                "--image",
+            ),
+        )
+
+        assert result.exit_code == 0
+        assert logfile.exists()
+        assert outfile.exists()
+        assert outfile.stat().st_size > 0
+        assert img_file.exists()
+        assert img_file.stat().st_size > 0
+        patch.assert_called_once()
