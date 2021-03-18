8c6f0d1aaf44d72f6673da4f44cd25f774d4727d
Timothy Click <tclick@okstate.edu>
2021-03-18 14:45:23 -0500

    test: add test for tleap function in utils module

--- tests/libs/test_utils.py
+++ tests/libs/test_utils.py
@@ -12,5 +12,33 @@
 #  TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
 #  THIS SOFTWARE.
 # --------------------------------------------------------------------------------------
+from pathlib import Path
 
+import numpy as np
 import pytest
+from numpy import testing
+from pytest_mock import MockerFixture
+
+from ambgen.libs import utils
+
+from ..datafile import PDB
+
+
+@pytest.mark.parametrize("template", "fixed solvate tleap".split())
+def test_tleap(template: str, mocker: MockerFixture, tmp_path: Path):
+    """
+    GIVEN template parameters
+    WHEN run_tleap is called
+    THEN an input file is created and tleap is run
+    """
+    subprocess = mocker.patch("subprocess.check_call")
+    infile = tmp_path.joinpath(Path(template).with_suffix(".in"))
+    logfile = tmp_path.joinpath(Path(template).with_suffix(".log"))
+
+    utils.run_tleap(infile, PDB, prefix=template, template=template, logfile=logfile.as_posix())
+
+    assert infile.exists()
+    assert infile.stat().st_size > 0
+    assert logfile.exists()
+    assert logfile.stat().st_size > 0
+    subprocess.assert_called_once()
