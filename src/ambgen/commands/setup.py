bae0b68d22742b872ea5a726fb8696b8bcd27f79
Timothy Click <thclick@umary.edu>
2025-05-16 17:30:24 -0500

    feat: add stub for setup subcommand
    Signed-off-by: Timothy Click <tclick@alumni.ou.edu>

---
+++ src/ambgen/commands/setup.py
@@ -0,0 +1,92 @@
+# ------------------------------------------------------------------------------
+#  mdsetup
+#  Copyright (c) 2025 Timothy H. Click, Ph.D.
+#
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  Redistributions of source code must retain the above copyright notice, this
+#  list of conditions and the following disclaimer.
+#
+#  Redistributions in binary form must reproduce the above copyright notice,
+#  this list of conditions and the following disclaimer in the documentation
+#  and/or other materials provided with the distribution.
+#
+#  Neither the name of the author nor the names of its contributors may be used
+#  to endorse or promote products derived from this software without specific
+#  prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR
+#  ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+#  OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+#  DAMAGE.
+# ------------------------------------------------------------------------------
+"""Create simulation subdirectories."""
+
+from pathlib import Path
+from typing import Annotated
+
+import typer
+
+from . import FILE_MODE
+
+DEFAULT_OUTDIR = Path.cwd()
+DEFAULT_LOGFILE = DEFAULT_OUTDIR / "setup.log"
+app = typer.Typer()
+
+
+@app.command(help="Create simulation subdirectories.")
+def setup(
+    logfile: Annotated[
+        typer.FileTextWrite,
+        typer.Option(
+            help="Log file",
+            show_default=True,
+        ),
+    ] = DEFAULT_LOGFILE,
+    outdir: Annotated[
+        Path,
+        typer.Option(
+            help="Output directory",
+            file_okay=False,
+            dir_okay=True,
+            resolve_path=True,
+            writable=True,
+            readable=True,
+            mode=FILE_MODE,
+            show_default=True,
+        ),
+    ] = DEFAULT_OUTDIR,
+) -> None:
+    """Create simulation subdirectories.
+
+    Parameters
+    ----------
+    logfile : typer.FileTextWrite, optional
+        th to the log file where logs will be written. If not specified, defaults to "setup.log" in the current
+        directory.
+    outdir : Path, optional
+        Path to the output directory where results or files will be stored. Must be a readable and writable
+        directory. Defaults to the current working directory.
+
+    Returns
+    -------
+    None
+        This function does not return any value. It performs setup operations such as preparing logging and validating
+        output paths.
+
+    Notes
+    -----
+    This function is intended to be used as a CLI command with `typer`. It uses type annotations and `typer.Option`
+    to define command-line options.
+    """
+    pass
